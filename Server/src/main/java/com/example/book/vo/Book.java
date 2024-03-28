26af83f155d7fa0084905bbc5b6beac25a2970a5
rjswh0503 <rjswh0503@yahoo.com>
2024-03-29 00:13:11 +0900

    Vo

--- Server/src/main/java/com/example/book/vo/Book.java
+++ Server/src/main/java/com/example/book/vo/Book.java
@@ -1,5 +1,30 @@
 package com.example.book.vo;
 
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Book {
 
+	private Long bookNo;
+	private String bookIsbn;
+	private String bookTitle;
+	private String bookContents;
+	private String bookInfoDate;
+	private String bookAuthor;
+	private String bookPublisher;
+	private String bookPrice;
+	private String bookCategory;
+	private Date bookDate;
+	private String bookImg;
+	private String bookAuthorc;
+	private String bookImg2;
+	
 }
